ba7962f3f6dec401e3ad3208d7d260adc72c6586
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-30 20:19:48 +0600

    basic data type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-const course = "Next Level Web Developer";
+// basic data type 
+
+//string 
+let firstname: string = "Ashraful";
+
+// number
+let roll:number  = 124
+
+// boolean 
+let isAdmin: boolean = true;
+
+// undefined 
+let x: undefined  = undefined;
+
+// null 
+let n : null = null;
+
+let d;
+
+d = 12
+d = '44'
+d = true;
+
+
+// array 
+
+// number type of array 
+let numbers: number[] = [24]
+
+
+// string type of array 
+let friends: string[] = ['Ashraful', "Siam"]
+
+
+// tuple 
+let coordinate: [number, number] = [1, 5]
+
+
+let ageName: [number, string] = [50, 'Mr. Sam']
