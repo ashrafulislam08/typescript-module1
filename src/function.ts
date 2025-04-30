2c5519361828a886d0a7b31e91909fb893d0ceaa
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-30 21:10:28 +0600

    function

---
+++ src/function.ts
@@ -0,0 +1,15 @@
+// Function 
+
+// Normal function 
+// Arrow function 
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+const sub = (a: number, b: number): number => a + b;
+
+
+
+const nums: number[] = [20, 14, 32, 25]
+
+const numberSquare = nums.map(num => num * num);
