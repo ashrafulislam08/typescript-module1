b17f2bcdc8e7c7977e05594f56bf29cb797ec216
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-30 22:27:50 +0600

    rest and spread operators

---
+++ src/spread_dest.ts
@@ -0,0 +1,11 @@
+// spread operator 
+// rest operator 
+// destructure 
+
+
+// spread operator 
+
+const   bros: string[] = ["Mir", "Firoz", "Mizan"]
+const   bros2: string[] = ["Tonmoy", "Majod", "Rahat"]
+
+bros.push(...bros2)
