137aff9a888eea18a6c41198cbe4a2346d1cdf14
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-01 10:47:19 +0600

    Array destructuring

--- src/spread_dest.ts
+++ src/spread_dest.ts
@@ -9,3 +9,39 @@ const   bros: string[] = ["Mir", "Firoz", "Mizan"]
 const   bros2: string[] = ["Tonmoy", "Majod", "Rahat"]
 
 bros.push(...bros2)
+
+
+const mentors1 = {
+    typecript: "Mezba",
+    redux: 'MiR',
+    dbms: 'Mizan'
+}
+
+const mentors2 = {
+    prisma: "Firoz",
+    next: "Tonmoy",
+    cloud: "Nahid",
+}
+
+const mentors = {
+    ...mentors1,
+    ...mentors2
+}
+
+
+// rest operator 
+const greetFriends = (...friends: string[]) => { 
+    console.log('Hi friends,', friends)
+}
+
+
+// destructuring
+const userOne = {
+    id: 1,
+    name: {
+        firstname: "Ashraful",
+        lastname: "Islam",
+    },
+    contactNo: '01555245547',
+    address :"Uganda"
+}
